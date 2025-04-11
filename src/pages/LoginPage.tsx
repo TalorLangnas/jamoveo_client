486cc4cb1e8abe04796811bbeede618928b9d467
TalorLangnas <118974915+TalorLangnas@users.noreply.github.com>
2025-04-11 18:05:40 +0300

    update before styling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 // src/pages/LoginPage.tsx
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";  
 import useAuth from "../hooks/useAuth";  
 import useAdminSession from "../hooks/useAdminSession";  
